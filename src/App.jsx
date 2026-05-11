a1e3276e38697902d5f51fb6e56f020835507ae3
lisboathecoder <gustavo.lisboa2009@gmail.com>
2026-05-11 10:35:57 -0300

    :tada: first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { BrowserRouter, Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Extra from "./pages/Extra";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+
